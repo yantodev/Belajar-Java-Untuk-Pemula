25a2442fa8289fe2f3963ae09e8d06c103101767
yantodev <ekocahyanto007@gmail.com>
2024-10-12 02:21:23 +0700

    Feat: Hari 1: Pengenalan Java dan Setup Lingkungan Pengembangan

---
+++ src/hari_ke_1/Main.java
@@ -0,0 +1,20 @@
+package hari_ke_1;
+import java.util.Scanner;
+
+//Hari Pertama
+
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+//    }
+//}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
